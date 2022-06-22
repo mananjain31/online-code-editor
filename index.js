859ea57ba7a843428e7e2b3d312ecd05c0f1e509
mananjain31 <mananjain31jan@gmail.com>
2022-06-23 01:26:30 +0530

    :technologist: request logging to console

--- index.js
+++ index.js
@@ -10,6 +10,18 @@ const codes = require('./routers/codes');
 
 app.use(cookieParser());
 app.use(express.json())
+app.use(function (req, res, next) {
+    console.log("----------REQUEST LOG---------");
+    console.log("req.method", req.method);
+    console.log("req.url", req.url);
+    console.log();
+    console.log("req.body", req.body);
+    console.log();
+    console.log("req.params", req.params);
+    console.log("-----------REQUEST LOG ENDS---");
+    console.log();
+    next();
+})
 app.use('/api/v1/auth', auth);
 app.use('/api/v1/codes', codes)
 
