b5258b08b8b5233f2b8575a0ba04b64a8863ecb6
mananjain31 <mananjain31jan@gmail.com>
2022-06-22 13:09:37 +0530

    :passport_control: added and implemented logout route

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,6 +2,7 @@ const User = require("../models/User");
 const jwt = require('jsonwebtoken');
 
 exports.signup = async (req, res) => {
+    console.log(req.body)
     const { username, email, password } = req.body;
     const user = await User.findOne({ email }) || await User.findOne({ username });
     if (user) {
@@ -15,8 +16,17 @@ exports.signup = async (req, res) => {
         email,
         password
     });
-    await newUser.save();
-    res.json({
+    try {
+        await newUser.save();
+    }
+    catch (error) {
+        console.log(error);
+        return res.status(400).send({
+            success: false,
+            message: 'Invalid Credentials'
+        })
+    }
+    res.status(201).json({
         success: true,
         message: 'User created successfully'
     });
@@ -39,12 +49,22 @@ exports.signin = async (req, res) => {
         });
     }
     const jwt_payload = {
-        username: user.username
+        username: user.username,
+        email: user.email,
     }
     const token = jwt.sign(jwt_payload, process.env.SECRET);
     res.cookie('token', token)
         .status(200).json({
             success: true,
-            message: 'User signed in successfully'
+            message: 'User signed in successfully',
+            token,
+        });
+}
+
+exports.logout = (req, res) => {
+    res.clearCookie('token')
+        .status(200).json({
+            success: true,
+            message: 'User signed out successfully',
         });
 }
--- routers/auth.js
+++ routers/auth.js
@@ -1,8 +1,9 @@
 const router = require("express").Router();
-const { signup, signin } = require('../controllers/auth');
+const { signup, signin, logout } = require('../controllers/auth');
 
 
-router.route('/signup').post(signup);
 router.route('/signin').post(signin);
+router.route('/signup').post(signup);
+router.route('/logout').get(logout);
 
 module.exports = router;
