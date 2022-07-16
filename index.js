ed5e2dbc35c677fe491f3c5a57dc9602406286a2
mananjain31 <mananjain31jan@gmail.com>
2022-07-16 17:12:03 +0530

    :hammer: serving static content which is build from react app

--- index.js
+++ index.js
@@ -2,12 +2,13 @@ require("dotenv").config();
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const { default: mongoose } = require('mongoose');
-const User = require("./models/User");
+const path = require("path");
 const app = express();
 const PORT = 8000;
 const auth = require('./routers/auth');
 const codes = require('./routers/codes');
 
+app.use(express.static(path.join(__dirname, 'build')));
 app.use(cookieParser());
 app.use(express.json())
 app.use(function (req, res, next) {
