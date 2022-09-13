b83c867dee8be9de6d7b448445e34f312ec8f485
mananjain31 <mananjain31jan@gmail.com>
2022-09-14 00:06:15 +0530

    minor changes

--- index.js
+++ index.js
@@ -7,8 +7,12 @@ const app = express();
 const PORT = process.env.PORT || 8000;
 const auth = require("./routers/auth");
 const codes = require("./routers/codes");
+const cors = require("cors");
+app.use(cors());
 
-app.use(express.static(path.join(__dirname, "build")));
+app.use(express.static(path.join(__dirname, "/build")));
+app.use(express.static(path.join(__dirname, "/dist")));
+app.use(express.static(path.join(__dirname, "/public")));
 app.use(cookieParser());
 app.use(express.json());
 app.use(function (req, res, next) {
